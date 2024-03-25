b6a243a1ec6565124de7ee95b8298e7a2c8e1619
Eduardo Lago Nunes <eduardolagonunes@gmail.com>
2024-03-25 11:05:48 -0300

    fixed scrool to top na troca para página de cursos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import CoursesPage from "./Pages/Courses/CoursesPage";
@@ -8,12 +8,23 @@ import Home from "./Pages/Home/Home";
 import "./index.css";
 import TypeCourseContextProvider from "./Context/typeCourseContext";
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
       <TypeCourseContextProvider>
         <Header />
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/courses" element={<CoursesPage />} />
